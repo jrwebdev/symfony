14286584b7465015786d9c305af18c0c9b412b64
jrwebdev <web@jravenscroft.co.uk>
2015-06-16 12:16:21 +1200

    Basic blog functionailty

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,14 +4,44 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+use AppBundle\Entity\Post;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/app/example", name="homepage")
+     * @Route("/", name="homepage")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('default/index.html.twig');
+        // Load all posts
+        $posts = $this->getDoctrine()->getRepository('AppBundle:Post')->findAll();
+
+        // Build form
+        $post = new Post();
+        $post->setDate(new \DateTime());
+
+        $form = $this->createFormBuilder($post)
+            ->add('title', 'text')
+            ->add('text', 'textarea')
+            ->add('save', 'submit', array('label' => 'Add Post'))
+            ->getForm();
+
+        // Process form
+        $form->handleRequest($request);
+
+
+        if ($form->isValid())
+        {
+            $em = $this->getDoctrine()->getEntityManager();
+            $em->persist($post);
+            $em->flush();
+        }
+
+        return $this->render('default/index.html.twig', array(
+            'posts' => $posts,
+            'form'  => $form->createView()
+        ));
     }
 }
